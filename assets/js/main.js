cbb1067c96a10bf01b2f6538beedebe3901fdfff
“rouzbehmozafari” <rouzbeh.mozafari@yahoo.com>
2021-11-24 14:32:44 +0100

    first

---
+++ assets/js/main.js
@@ -0,0 +1,135 @@
+// let land = ['nrw' , 'hamburg' , 'berlin']
+
+// let n = land.map(elem => elem.toUpperCase())
+// console.log(n)
+// let m = land.map(elem => elem[0].toUpperCase()+elem.slice(1))
+// console.log(m)
+
+// let numArray2 = ["Sergio", "Hannah", "Regallager", "Reliefpfeiler", "Rentner"];
+// let rev = (ar) => {
+//     let n = ar.map(x => x.split('') , x => x.reverse() )
+//     n = n.map(x => x.reverse())
+//     n = n.map( x => x.join(''))
+//     console.log(n)
+//     return n 
+// };
+// rev(numArray2)
+
+// let getraenke = [
+//     "Coca-Cola",
+//     "Apfelsaft",
+//     "Pepsi",
+//     "Traubensaft",
+//     "Sprite",
+//     "Orangensaft",
+//     "Red Bull Energy Drink",
+//     "Fanta"
+//  ]
+ 
+// // getraenke.sort() 
+// // getraenke.forEach((x)=> document.body.innerHTML += '<h1>'+ x + '</h1>')
+// // console.log(getraenke)
+// let n = getraenke.map(x => x.toUpperCase())
+// console.log(n)
+// let array = [
+//     18,
+//     16,
+//     80,
+//     51,
+//     47,
+//     38,
+//     95,
+//     42,
+//     68,
+//     61,
+//     34,
+//     51,
+//     20,
+//     17,
+//     56,
+//     31,
+//     100,
+//     6,
+//     5,
+//     30,
+//     74,
+//     97,
+//     28,
+//     99,
+//     91,
+//     27,
+//     73,
+//     12,
+//     92,
+//     6,
+//     27,
+//     71,
+//     26,
+//     15,
+//     78
+//   ];
+// function my(x) {
+//     x = x.map(x => x*2)
+//     x.sort((a,b) => a - b)
+//     return x
+// }  
+// console.log(my(array))
+
+let checkNumber = [
+    18,
+    16,
+    80,
+    51,
+    47,
+    38,
+    95,
+    42,
+    68,
+    61,
+    34,
+    51,
+    20,
+    17,
+    56,
+    31,
+    100,
+    6,
+    5,
+    30,
+    74,
+    97,
+    28,
+    99,
+    91,
+    27,
+    73,
+    12,
+    92,
+    6,
+    27,
+    71,
+    26,
+    15,
+    78
+  ];
+// let ch = (x) => {
+//     let ar = []
+//     if (x % 3 ==0) {
+//         return x+100
+//     }
+// }
+// let ar = []
+// checkNumber.forEach(function(x){
+//     if (x % 3 ==0){
+//         ar.push(x + 100)
+//     }
+// })
+// console.log(ar)
+// let country = ['france' , 'germany' , 'spain']
+
+// for (let i = 0; i < country.length; i++) {
+//     console.log(country[i].charAt(0).toUpperCase() + country[i].slice(1))
+    // for (let x = 0; x < 2; x++) {
+    //     console.log(country[i])
+    // }
+    
